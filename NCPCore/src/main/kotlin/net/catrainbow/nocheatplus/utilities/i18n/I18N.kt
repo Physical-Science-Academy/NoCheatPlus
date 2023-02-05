1c87ee1a73163b0b916b05d0612fb05ddc028ff6
Verox001 <verox@cimeyclust.com>
2023-02-05 17:17:45 +0100

    Revert "Added waterdog pe dependency to project relative path (#12)"
    This reverts commit dc9bdb078b14505249ae8aaa3f424295c97e817f.

--- NCPCore/src/main/kotlin/net/catrainbow/nocheatplus/utilities/i18n/I18N.kt
+++ NCPCore/src/main/kotlin/net/catrainbow/nocheatplus/utilities/i18n/I18N.kt
@@ -13,14 +13,8 @@ import java.util.Properties
  */
 class I18N {
     companion object {
-        private var language: String = "zh"
-
-        fun updateLanguage(languageKey: String) {
-            language = languageKey
-        }
-
         private var translations =
-            Properties().apply { load(FileInputStream("${NoCheatPlus.instance.dataFolder}/lang/$language.properties")) }
+            Properties().apply { load(FileInputStream("${NoCheatPlus.instance.dataFolder}/translations.properties")) }
 
         @JvmStatic
         fun getString(key: String, vararg args: Any): String {
